64429944fb277a26efaba0cb7a99fcbf1e5da9c4
stonefruit <jeromeng888@gmail.com>
2022-07-09 22:50:12 +0800

    add release version

--- build-single-file-new-version.sh
+++ build-single-file-new-version.sh
@@ -4,21 +4,10 @@ new_version=$(( prev_version + 1 ))
 npm run build && npx gulp
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' -e "s/__BUILD__/v$new_version/" build/index.html 
+  sed -i '' -e "s/__BUILD__/$RELEASE_VERSION/" build/index.html 
 else
-  sed -i -e "s/__BUILD__/v$new_version/" build/index.html 
+  sed -i -e "s/__BUILD__/$RELEASE_VERSION/" build/index.html 
 fi
 
-# Checksum generation. Verify by running next 2 commands with 
-# the sample file and compare first 3 letters
-sha=($(shasum build/index.html ))
-checksum_first_3=$(echo $sha |cut -c1-3)
-filename="legendlist_v"$new_version".html"
-
 mkdir -p builds
-cp build/index.html builds/$filename && rm -rf build
-
-echo $new_version > VERSION.txt
-
-echo "Copy and paste the path on a browser to view:"
-echo $PWD/builds/$filename
+cp build/index.html builds/legendlist.html && rm -rf build
